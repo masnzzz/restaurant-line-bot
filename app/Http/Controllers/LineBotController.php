cc071d6d00fcc247b6437d30e816948b98d1021c
MasanaIto <z3heng0326@gmail.com>
2020-03-12 11:00:54 +0900

    署名の検証を行う
    LINEチャネルからのリクエストに含まれる署名(signature)の情報を検証する

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -21,5 +21,10 @@ class LineBotController extends Controller
 
         $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
         $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
     }
 }
