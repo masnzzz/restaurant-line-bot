676840c540c605ff723c20ae454b5fe08b6b09d8
MasanaIto <z3heng0326@gmail.com>
2020-03-12 10:51:24 +0900

    parrotメソッド作成、ログ設定を追加
    LINEBotクラスを生成する

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineBotController extends Controller
 {
     public function index()
     {
         return view('linebot.index');
+    }
+
+    public function parrot(Request $request)
+    {
+        Log::debug($request->header());
+        Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
     }
 }
